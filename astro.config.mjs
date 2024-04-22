6949e9cc4ddd503a5951a77dc75185cba57f367c
Francisco <javiermartincuenca@gmail.com>
2024-04-22 18:56:42 +0200

    Deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs(), tailwind()],
+  site: 'https://franciscojaviermartin.github.io',
+  base: 'airbnb-tailwind',
 });
