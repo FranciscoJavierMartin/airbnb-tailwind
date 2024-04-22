19c6a808316e1e451d6b81bd1898495d70fc0c10
Francisco <javiermartincuenca@gmail.com>
2024-04-22 19:20:30 +0200

    Customize tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#222222',
+        },
+
+        airbnb: {
+          accent: '#ff5a5f',
+          green: '#00a699',
+          orange: '#fc642d',
+          gray: {
+            light: '#767676',
+            dark: '#484848',
+          },
+        },
+      },
+      fontFamily: {
+        text: ['Nunito', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
